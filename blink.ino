f19dfc4202aa3563a53a19c5edd535d77a4e685e
Janek Nowak <44327230+wojciech-moch@users.noreply.github.com>
2022-04-18 22:59:15 +0200

    Przykład kodu błyskającego (na bazie arduino.cc)

---
+++ blink.ino
@@ -0,0 +1,24 @@
+/*
+  Blink
+  Włącza diodę LED na sekundę, potem wyłącza ją na sekundę i powtarza całość.
+ 
+  Ten przyklad należy do domeny publicznej.
+ */
+ 
+// W większości płytek Ardiono do pinu 13 podłączona jest dioda LED.
+// nadajmy jej nazwę:
+int led = 13;
+
+// Procedura setup uruchamiana jest raz, po naciśnięciu przycisku reset:
+void setup() {                
+  // inicjowanie cyfrowego pinu jako wyjścia.
+  pinMode(led, OUTPUT);     
+}
+
+// procedura loop jesty wykonywana w nieskonczoność:
+void loop() {
+  digitalWrite(led, HIGH);   // włączenie diody LED (wysoki poziom napięcia)
+  delay(1000);               // odczekanie sekundy
+  digitalWrite(led, LOW);    // wyłączenie diody LED (niski poziom napięcia)
+  delay(1000);               // odczekanie sekundy
+}
